ad1e91587a3eebb69441b1d4004a070320b4aca7
ajinra020307 <ajinra020307@gmail.com>
2019-08-04 11:02:43 +0530

    d

--- js/home.js
+++ js/home.js
@@ -1,4 +1,16 @@
 const buttons=document.querySelectorAll('button')
 buttons.forEach((v)=>{
-	v.classList.add("animated","heartBeat","slower","infinite")
+	v.classList.add("animated","pulse","slow","infinite")
 })
+
+function displaycontact(){
+	document.querySelector('.showcase').style.display="none"
+	document.querySelector('.top_collections').style.display="none"
+	document.querySelector('.contact').style.display="flex"
+}
+
+function removecontact(){
+	document.querySelector('.showcase').style.display="flex"
+	document.querySelector('.top_collections').style.display="block"
+	document.querySelector('.contact').style.display="none"
+}
