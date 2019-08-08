54c93d42030619f917bce14dde48ddca77d021cd
ajinra020307 <ajinra020307@gmail.com>
2019-08-08 16:38:46 +0530

    s

--- js/home.js
+++ js/home.js
@@ -4,25 +4,3 @@ buttons.forEach((v)=>{
 	   v.style.backgroundColor="green"
 	});
 })
-
-function displaycontact(){
-	try{
-		document.querySelector('.showcase').style.display="none"
-	document.querySelector('.top_collections').style.display="none"
-	document.querySelector('.contact').style.display="flex"
-}catch(e){
-
-}
-	
-}
-
-function removecontact(){
-	try{
-		document.querySelector('.showcase').style.display="flex"
-	document.querySelector('.top_collections').style.display="block"
-	document.querySelector('.contact').style.display="none"
-	}catch(e){
-		
-	}
-	
-}
